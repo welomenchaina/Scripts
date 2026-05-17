765702ce8909225293b12128cce2a665a7a268bf
Hydrox <139654803+welomenchaina@users.noreply.github.com>
2026-05-17 16:17:23 -0500

    Update HookMetaMethod.lua

--- Functions/HookMetaMethod.lua
+++ Functions/HookMetaMethod.lua
@@ -1,89 +1,56 @@
-local HookMetaMethodModule = {}
+local Module = {}
 
-local blockedRemotes = {}
-local hookedRemotes = {}
-local lastArgs = {}
-local resumedRemotes = {}
+local hooks = {}
+local blocked = {}
 
-local function resolveRemote(remote)
-	if typeof(remote) == "string" then
-		local obj = game
-		for part in string.gmatch(remote, "[^%.]+") do
-			obj = obj:FindFirstChild(part)
-			if not obj then return nil end
-		end
-		return obj
-	end
-	return remote
-end
+local mt = getrawmetatable(game)
+local old = mt.__namecall
 
-function HookMetaMethodModule.New(remote, ...)
-	remote = resolveRemote(remote)
-	if not remote or typeof(remote) ~= "Instance" then return end
-	if hookedRemotes[remote] then return end
+setreadonly(mt, false)
+
+mt.__namecall = newcclosure(function(self, ...)
 	local args = {...}
-	local oldFireServer
-	oldFireServer = hookfunction(remote.FireServer, function(self, ...)
-		if blockedRemotes[self] then return end
-		local callArgs = table.pack(...)
-		for i, v in ipairs(args) do
-			callArgs[i] = v
+	local method = getnamecallmethod()
+
+	if method == "FireServer" then
+		local data = hooks[self]
+
+		if blocked[self] then
+			return nil
 		end
-		lastArgs[self] = callArgs
-		return oldFireServer(self, table.unpack(callArgs, 1, callArgs.n))
-	end)
-	hookedRemotes[remote] = oldFireServer
-end
 
-function HookMetaMethodModule.Block(remote)
-	remote = resolveRemote(remote)
-	if remote then
-		blockedRemotes[remote] = true
-	end
-end
-
-function HookMetaMethodModule.Resume(remote)
-	remote = resolveRemote(remote)
-	if remote then
-		blockedRemotes[remote] = nil
-		resumedRemotes[remote] = true
-	end
-end
-
-function HookMetaMethodModule.Unhook(remote)
-	remote = resolveRemote(remote)
-	if remote and hookedRemotes[remote] then
-		local old = hookedRemotes[remote]
-		hookfunction(remote.FireServer, old)
-		hookedRemotes[remote] = nil
-		lastArgs[remote] = nil
-	end
-end
-
-function HookMetaMethodModule.CallLastArgs(remote)
-	remote = resolveRemote(remote)
-	if remote and lastArgs[remote] and not blockedRemotes[remote] then
-		remote:FireServer(table.unpack(lastArgs[remote], 1, lastArgs[remote].n))
-	end
-end
-
-function HookMetaMethodModule.Hook(property)
-	if not property then return end
-	property = tostring(property)
-	if property:lower() == "walkspeed" then
-		local mt = getrawmetatable(game.Players.LocalPlayer)
-		local old = mt.__newindex
-		setreadonly(mt, false)
-		mt.__newindex = function(self, k, v)
-			if k:lower() == "walkspeed" then
+		if data then
+			for i,v in pairs(data.Args) do
+				args[i] = v
 			end
-			return old(self, k, v)
+			return old(self, unpack(args))
 		end
-		setreadonly(mt, true)
 	end
+
+	return old(self, ...)
+end)
+
+setreadonly(mt, true)
+
+function Module.New(remote, ...)
+	hooks[remote] = {
+		Args = {...}
+	}
 end
 
-return HookMetaMethodModule
+function Module.Block(remote)
+	blocked[remote] = true
+end
+
+function Module.Resume(remote)
+	blocked[remote] = nil
+end
+
+function Module.Unhook(remote)
+	hooks[remote] = nil
+end
+
+return Module
 
 --[[
 HookMetaMethodModule Usage:
